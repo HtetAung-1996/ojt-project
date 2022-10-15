d738e35d1f5497756d8af9620d71afc6ef60322b
Nlhmmh <nlh2012.8@gmail.com>
2022-10-15 13:52:12 +0900

    commit 1

--- backend/src/main/java/com/BScamp/MovieTheater/controller/UserController.java
+++ backend/src/main/java/com/BScamp/MovieTheater/controller/UserController.java
@@ -2,6 +2,8 @@ package com.BScamp.MovieTheater.controller;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,6 +11,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,9 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.BScamp.MovieTheater.entity.JwtResponse;
 import com.BScamp.MovieTheater.entity.LoginRequest;
 import com.BScamp.MovieTheater.entity.User;
 import com.BScamp.MovieTheater.entity.UserRole;
+import com.BScamp.MovieTheater.security.JwtUtils;
+import com.BScamp.MovieTheater.security.UserDetailsImpl;
 import com.BScamp.MovieTheater.service.UserService;
 
 @RestController
@@ -27,14 +36,18 @@ import com.BScamp.MovieTheater.service.UserService;
 public class UserController {
 
 	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@Autowired
 	UserService userService;
 
 	@PostMapping("/login")
-	public ResponseEntity<User> login(
-			@Valid @RequestBody LoginRequest lognReq, HttpSession session
-	) throws IOException {
-		User user = userService
-				.checkLoginUser(lognReq.getGmail(), lognReq.getPassword());
+	public ResponseEntity<User> login(@Valid @RequestBody LoginRequest lognReq, HttpSession session)
+			throws IOException {
+		User user = userService.checkLoginUser(lognReq.getGmail(), lognReq.getPassword());
 		if (user == null) {
 			return ResponseEntity.notFound().build();
 		}
@@ -42,9 +55,7 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public ResponseEntity<User> register(
-			@Valid @RequestBody User user, HttpSession session
-	) throws IOException {
+	public ResponseEntity<User> register(@Valid @RequestBody User user, HttpSession session) throws IOException {
 		user.setRole(UserRole.user);
 		user.setStartJoinDate(LocalDate.now());
 		User createdUser = userService.create(user);
@@ -53,8 +64,7 @@ public class UserController {
 	}
 
 	@GetMapping("/logout")
-	public Boolean logout(HttpSession session, HttpServletResponse response)
-			throws IOException {
+	public Boolean logout(HttpSession session, HttpServletResponse response) throws IOException {
 		session.invalidate();
 		return true;
 	}
@@ -67,6 +77,22 @@ public class UserController {
 	@PutMapping("/profile/update")
 	public User getProfile(@RequestBody User user) throws IOException {
 		return userService.update(user.getId(), user);
+	}
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> signIn(@Valid @RequestBody LoginRequest lognReq) throws IOException {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(lognReq.getGmail(), lognReq.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(
+				new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
 	}
 
 }
--- backend/src/main/java/com/BScamp/MovieTheater/security/WebSecurityConfig.java
+++ backend/src/main/java/com/BScamp/MovieTheater/security/WebSecurityConfig.java
@@ -1,39 +1,50 @@
 package com.BScamp.MovieTheater.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().mvcMatchers("/**").permitAll()
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
 
-		// .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		// .mvcMatchers("/", "/user/register", "/user/login").permitAll()
-		// .mvcMatchers("/logout", "/admin/**").hasRole("ADMIN")
-		// .anyRequest().authenticated().and()
-		// .formLogin().loginPage("/user/login").defaultSuccessUrl("/").and()
-		// .logout().invalidateHttpSession(true).logoutSuccessUrl("/")
-		;
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
 	}
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.debug(false).ignoring()
-				.antMatchers("/images/**", "/js/**", "/css/**");
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
 	}
 
 	@Bean
@@ -41,20 +52,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	// private final UserDetailsService userDetailsService = null;
-	//
-	// private final BCryptPasswordEncoder bCryptPasswordEncoder = new
-	// BCryptPasswordEncoder();
-	//
-	// @Override
-	// protected void configure(AuthenticationManagerBuilder auth) throws
-	// Exception {
-	// auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-	// }
-	//
-	// @Override
-	// protected void configure(HttpSecurity http) throws Exception {
-	// super.configure(http);
-	// }
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable().authorizeRequests().mvcMatchers("/**").permitAll()
+//
+//		// .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//		// .mvcMatchers("/", "/user/register", "/user/login").permitAll()
+//		// .mvcMatchers("/logout", "/admin/**").hasRole("ADMIN")
+//		// .anyRequest().authenticated().and()
+//		// .formLogin().loginPage("/user/login").defaultSuccessUrl("/").and()
+//		// .logout().invalidateHttpSession(true).logoutSuccessUrl("/")
+//		;
+//	}
+//
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//		web.debug(false).ignoring().antMatchers("/images/**", "/js/**", "/css/**");
+//	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/user/**").permitAll().antMatchers("/api/test/**").permitAll().anyRequest().authenticated();
+
+		http.authenticationProvider(authenticationProvider());
+
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
 
 }
