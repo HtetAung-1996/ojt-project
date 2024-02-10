09f40496ccbe498b67524c40ad3518f075f4f553
Nlhmmh <nlh2012.8@gmail.com>
2024-02-10 13:03:29 +0630

    fix register

--- frontend/src/views/register.vue
+++ frontend/src/views/register.vue
@@ -6,49 +6,58 @@
         <!-- Name -->
         <v-text-field
           v-model="name"
-          :counter="10"
           :rules="[
-            (v) => !!v || 'Required',
-            (v) =>
-              (v && v.length <= 10) || 'Name must be less than 10 characters',
+            (v) => !!v || '必須',
+            (v) => (v && v.length <= 30) || '最大30文字以下入力してください',
           ]"
-          label="Name"
+          label="名前"
           required
         ></v-text-field>
 
         <!-- Email -->
         <v-text-field
           v-model="email"
+          class="mt-1"
           :rules="[
-            (v) => !!v || 'Required',
-            (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            (v) => !!v || '必須',
+            (v) => (v && v.length <= 100) || '最大100文字以下入力してください',
+            (v) =>
+              /.+@.+\..+/.test(v) ||
+              'メールアドレスのフォマードが正しくありません',
           ]"
-          label="E-mail"
+          label="メール"
           required
         ></v-text-field>
 
         <!-- Password -->
         <v-text-field
           v-model="password"
-          :counter="10"
+          class="mt-1"
           :rules="[
-            (v) => !!v || 'Required',
-            (v) =>
-              (v && v.length <= 10) ||
-              'Password must be less than 10 characters',
+            (v) => !!v || '必須',
+            (v) => (v && v.length <= 100) || '最大100文字以下入力してください',
+            (v) => /[A-Z]/.test(v) || '一文字以上大文字を含む入力してください',
           ]"
-          label="Password"
+          label="パスワード"
+          required
+        ></v-text-field>
+
+        <!-- Password ReEnter -->
+        <v-text-field
+          v-model="passwordReEnter"
+          class="mt-1"
+          :rules="[
+            (v) => !!v || '必須',
+            (v) =>
+              v === password || 'パスワードと一致している値を入力してください',
+          ]"
+          label="パスワード再入力"
           required
         ></v-text-field>
 
         <!-- Register Btn -->
-        <v-btn
-          :disabled="!registerForm"
-          color="success"
-          class="mr-4"
-          @click="register()"
-        >
-          <span v-if="!loading">Register</span>
+        <v-btn color="success" class="mt-5 mr-4" @click="register()">
+          <span v-if="!loading">登録</span>
           <v-progress-circular
             v-else
             indeterminate
@@ -58,7 +67,7 @@
 
         <!-- Error Alert -->
         <v-alert class="mt-3" v-show="errorAlert" dense type="error">
-          Register Failed!
+          登録処理が失敗しました
         </v-alert>
       </v-form>
     </v-container>
@@ -77,12 +86,14 @@ export default {
     return {
       registerForm: false,
 
-      name: "",
-      email: "",
-      password: "",
-      // name: "Mg Mg",
-      // email: "test@gmail.com",
-      // password: "1111",
+      // name: "",
+      // email: "",
+      // password: "",
+      // passwordReEnter: "",
+      name: "Mg Mg",
+      email: "test@gmail.com",
+      password: "A1111",
+      passwordReEnter: "A1111",
 
       errorAlert: false,
       loading: false,
@@ -93,39 +104,42 @@ export default {
 
   methods: {
     async register() {
-      if (this.$refs.registerForm.validate()) {
+      if (!this.$refs.registerForm.validate()) return;
+      try {
         this.errorAlert = false;
-
-        try {
-          this.loading = true;
-
-          // API Call
-          const resp = await utils.http.post("/user/register", {
-            name: this.name,
-            gmail: this.email,
-            password: this.password,
-          });
-          if (resp.status === 200) {
-            const data = await resp.json();
-            if (data) {
-              // Store Login Info in Vuex
-              this.$store.commit("setLoginUser", data);
-              // If Admin -> Go to Admin
-              // If User -> Go to Home
-              if (data.role == "admin") {
-                this.$router.push({ path: "/admin" });
-              } else {
-                this.$router.push({ path: "/" });
-              }
-            }
-          } else {
-            this.errorAlert = true;
-          }
-        } catch (error) {
-          console.log(error);
-        } finally {
-          this.loading = false;
+        this.loading = true;
+        // API Call
+        const resp = await utils.http.post("/user/register", {
+          name: this.name,
+          gmail: this.email,
+          password: this.password,
+        });
+        if (!resp) {
+          this.errorAlert = true;
+          return;
         }
+        if (resp.status !== 200) {
+          this.errorAlert = true;
+          return;
+        }
+        const data = await resp.json();
+        if (!data) {
+          this.errorAlert = true;
+          return;
+        }
+        // Store Login Info in Vuex
+        this.$store.commit("setLoginUser", data);
+        // If Admin -> Go to Admin
+        // If User -> Go to Home
+        if (data.role == "admin") {
+          this.$router.push({ path: "/admin" });
+          return;
+        }
+        this.$router.push({ path: "/" });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.loading = false;
       }
     },
   },
